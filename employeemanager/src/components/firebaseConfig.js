8589a6b9571bd8846a8645bf45eac9e2f3126580
Kachi123 <kachi.erugo@codex.com.ng>
2020-07-16 12:05:20 +0100

    initial commit

---
+++ employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyByZpMQ6zAA-vMYisjlATNQRA1ozmlTBs4",
+    authDomain: "vuefs-prod-d7647.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-d7647.firebaseio.com",
+    projectId: "vuefs-prod-d7647",
+    storageBucket: "vuefs-prod-d7647.appspot.com",
+    messagingSenderId: "419587248472",
+    appId: "1:419587248472:web:21c5bb22a51ffb61bf552d",
+    measurementId: "G-DREZV3JFDQ"
+}
